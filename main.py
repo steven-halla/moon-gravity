46c115c6050b0f6c2b03028aa03c1382e8447038
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-19 12:00:41 -0700

    finished

--- main.py
+++ main.py
@@ -8,3 +8,17 @@
 # Version: 1.0
 # Date: April 19th 2023
 ################################################################
+
+# These Constants define the gravitational pull for earth and the moon
+EARTH_GRAVITY: float = 32.17  # feet/second/second
+MOON_GRAVITY: float = 5.3  # feet/second/second
+
+# This variable gets the users weight on earth
+userWeightOnEarth: float = float(input("Enter your weight on Earth (in pounds): "))
+
+# This variable gets the users weight on earth
+userWeightOnMoon: float = (userWeightOnEarth / EARTH_GRAVITY) * MOON_GRAVITY
+
+# display the results
+print(f"Your weight on Earth is: {userWeightOnEarth}")
+print(f"Your weight on moon is: {userWeightOnMoon}")
