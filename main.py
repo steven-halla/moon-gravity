f66d15e3d9ac8d1bd5dfe0da5a0fe8026ff9b459
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-19 11:38:47 -0700

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+################################################################
+# Project: Weight on the moon
+# File: main.py
+# Description: This program asks user to input their body weight, which then
+#does a calculation based on moon gravity to get the number of pounds a user
+#would weigh on the moon.
+# Author: Steven Halla
+# Version: 1.0
+# Date: April 19th 2023
+################################################################
